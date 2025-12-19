367a82aada3927a63c4ff707df3f62172a3273ce
Jorlan Heider <jorlan25.js@gmail.com>
2025-12-18 22:43:02 -0300

    up

---
+++ miner-precatorios/src/main/java/com/miner/precatorios/MinerPrecatoriosApplication.java
@@ -0,0 +1,13 @@
+package com.miner.precatorios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinerPrecatoriosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinerPrecatoriosApplication.class, args);
+	}
+
+}
---
+++ miner-precatorios/src/test/java/com/miner/precatorios/MinerPrecatoriosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miner.precatorios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinerPrecatoriosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
